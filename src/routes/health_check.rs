d9f28f73f00420eb95e11354723c3af4c889ad2e
Gary Holland <garyhollandxyz@gmail.com>
2020-11-10 08:18:51 +0000

    add tracing span for health_check

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -1,5 +1,6 @@
 use actix_web::{HttpRequest, HttpResponse};
 
+#[tracing::instrument(name = "Checking application health", skip(_req))]
 pub async fn health_check(_req: HttpRequest) -> HttpResponse {
     println!("In health_check!");
     HttpResponse::Ok().finish()
