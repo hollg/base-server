60a945240214d3565c870d3fe220867ab114d04d
Gary Holland <garyhollandxyz@gmail.com>
2020-11-10 14:27:56 +0000

    specify db host in github actions workflow

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -6,8 +6,10 @@ set -eo pipefail
 DB_USER=${POSTGRES_USER:=postgres}
 # Check if a custom password has been set, otherwise default to 'password'
 DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
-# Check if a custom password has been set, otherwise default to 'newsletter'
-DB_NAME="${POSTGRES_DB:=newsletter}"
+# Check if a custom database name has been set, otherwise default to 'example'
+DB_NAME="${POSTGRES_DB:=example}"
+# Check if a custom host has been set, otherwise default to 'localhost'
+DB_HOST="${POSTGRES_HOST:=localhost}"
 # Check if a custom port has been set, otherwise default to '5432'
 DB_PORT="${POSTGRES_PORT:=5432}"
 
@@ -25,7 +27,7 @@ if [[ -z "${SKIP_DOCKER}" ]]; then
 fi
 
 # Keep pinging Postgres until it's ready to accept commands
-until PGPASSWORD="${DB_PASSWORD}" psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
     echo >&2 "Postgres is still unavailable - sleeping"
     sleep 1
 done
