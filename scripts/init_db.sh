794bdbe0d095a0a4c3ceb4903e57a00a4e411cd5
Gary Holland <garyhollandxyz@gmail.com>
2020-11-12 19:06:56 +0000

    correct guard

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -36,7 +36,7 @@ echo >&2 "Postgres is up and running on port ${DB_PORT} - running migrations now
 
 export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 # For GitHub Actions workflow
-if [$CI == true]; then
+if [["${CI}"]]; then
     echo "DATABASE_URL=${DATABASE_URL}" >>$GITHUB_ENV
 fi
 
